57d7b49a7ddca872686bfe2122c0d71a6f270a2c
LaoCC <fazo@qq.com>
2022-04-15 01:13:03 +0800

    Signed-off-by: LaoCC <fazo@qq.com>

--- src/mysql/Cache.php
+++ src/mysql/Cache.php
@@ -32,13 +32,14 @@ final class Cache
 
     public function get(string $key)
     {
-        return $this->redis->hGet($this->hashKey, $key);
+        $mdKey = ($this->table . '_' . $key);
+        return $this->redis->hGet($this->hashKey, $mdKey);
     }
-
 
     public function set(string $key, $value)
     {
-        return $this->redis->hSet($this->hashKey, $key, $value);
+        $mdKey = ($this->table . '_' . $key);
+        return $this->redis->hSet($this->hashKey, $mdKey, $value);
     }
 
 
