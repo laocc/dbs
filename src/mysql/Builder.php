cec070116d0a63b2d7e80a9411ff77a0d752a449
老船长 <fazo@qq.com>
2025-03-05 23:09:42 +0800

    Default Changelist

--- src/mysql/Builder.php
+++ src/mysql/Builder.php
@@ -1593,7 +1593,7 @@ final class Builder
                     $value = $this->quote($value);
                 }
 
-            } elseif (in_array($kFH, ['+', '-', '|', '$', '!'])) { //键以+-结束，或以|&!$结束的位运算
+            } elseif (in_array($kFH, ['+', '-', '|', '$', '!', '^'])) { //键以+-结束，或以|&!$结束的位运算
                 $key = substr($key, 0, -1);
                 if (!is_numeric($value)) throw new Error("DB_ERROR: [{$key}]加减操作时，其值必须为数字", $tractLevel + 1);
                 if ($value < 0) throw new Error("DB_ERROR: [{$key}]加减操作时，其值必须为非负数", $tractLevel + 1);
